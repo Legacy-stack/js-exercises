4c1ca9f8a839d54625d6cbab5229b731313bd7a0
Ruth <aylaaeternum@gmail.com>
2021-10-04 13:25:09 +0200

    update calculator

--- js/calculatorPractice.js
+++ js/calculatorPractice.js
@@ -4,16 +4,30 @@
 
 //se declara la función. (quitamos los parámetros) 
 
-var operaciones = function () {
+var operaciones = function (e) {
 
-var numero1 = parsefloat(document.getElementById("numero1").value); // al poner el .value obtengo el valor del elemento. funcion Parsefloat para que lo identifique como un número
-var numero2 = parsefloat(document.getElementById("numero2").value);
+var numero1 = parseFloat(document.getElementById("numero1").value); // al poner el .value obtengo el valor del elemento. funcion Parsefloat para que lo identifique como un número
+var numero2 = parseFloat(document.getElementById("numero2").value);
 var result = document.getElementById("resultado");
 
-var resultadoFinal =numero1 + numero2;
-result.innerHTML = resultadoFinal; 
+var operacion = e.target.id;
+
+    if (operacion == "suma") {
+        var resultado = numero1 + numero2;
+    }
+    else if (operacion == "resta") {
+        var resultado = numero1 - numero2;
+    }
+    else if (operacion == "multiplica") {
+        var resultado = numero1 * numero2;
+    }
+    else if (operacion == "divide") {
+        var resultado = numero1 / numero2;
+    }
+
+    result.innerHTML = resultado;
 }
 
 //Se aplica el evento a la variable (boton en este caso). Tiene dos valores, el evento y la funcion que quiero que se ejecute
-var boton = document.getElementById("suma");
-boton.addEventListener("click", operaciones);
+var botones = document.getElementById("btns"); // es el id del padre
+botones.addEventListener("click", operaciones);
