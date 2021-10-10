19b57c2653e04307b1880bda2d387d281adceb10
Ruth <aylaaeternum@gmail.com>
2021-10-10 11:09:30 +0200

    valores

--- js/newclass.js
+++ js/newclass.js
@@ -1,3 +1,18 @@
-function name(params) {
-    
-}
+//Valores
+
+//Un string. Es una cadena de texto
+"Ruth"
+
+//Valores numericos
+
+12
+12.1
+
+//valores booleanos
+
+true
+false
+
+//Valores vacios
+null
+undefined // valor indefinido pero que se va a definir mas adelante
