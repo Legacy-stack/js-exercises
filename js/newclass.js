1c4b64c7cb76c7d1e242b0bbc85e46e738078e28
Ruth <aylaaeternum@gmail.com>
2021-10-10 11:48:58 +0200

    classchanges

--- js/newclass.js
+++ js/newclass.js
@@ -16,3 +16,25 @@ false
 //Valores vacios
 null
 undefined // valor indefinido pero que se va a definir mas adelante
+
+//Variables. almacenan y registran los valores. 
+var let //const   // la que más se usa ahora es let y const para las inmutables
+
+var nombre = "Ruth"; //  ; Significa que aquí termina esta declaración
+var edad = 35;  // Dato numerico. No llevan comillas.
+
+// "Ruth" es el string. var es la variable, nombre es el nombre de la variable
+
+// Como se declara una variable
+
+var edad; // y ya, así solo está almacenando un nombre, pero está undefined. NO tiene valor. Para inicializarla hay que darle un valor
+edad = 23;
+
+//variables complejas o TIPO OBJETO. Se pueden almacenar diferentes propiedades, en este caso son diferentes propiedades de la variable "persona"
+
+var persona = {
+    nombre: "Ruth",
+    apellido: "Moya",
+    edad: 37
+}
+
